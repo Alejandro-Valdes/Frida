8e163eafb209febaef1cba5d7e5fad7ade7fb2ed
Alejandro <alejandro.valdesval@gmail.com>
2017-03-19 15:19:42 -0600

    change semantic cube to standard spanish types

--- semantic_cube.py
+++ semantic_cube.py
@@ -2,85 +2,85 @@
 VOID = 0
 BOOL = 1
 BOOLARRAY = 11
-INT = 2
-INTARRAY = 22
-FLOAT = 3
-FLOATARRAY = 33
-STRING = 4
-STRINGARRAY = 44
+ENTERO = 2
+ENTEROARRAY = 22
+DECIMAL = 3
+DECIMALARRAY = 33
+CADENA = 4
+CADENAARRAY = 44
 
 
 cubeDict = {}
 
 # Assignments
 cubeDict['bool=bool'] = BOOL
-cubeDict['int=int'] = INT
-cubeDict['float=float'] = FLOAT
-cubeDict['string=string'] = STRING
-cubeDict['float=int'] = FLOAT
+cubeDict['entero=entero'] = ENTERO
+cubeDict['decimal=decimal'] = DECIMAL
+cubeDict['cadena=cadena'] = CADENA
+cubeDict['decimal=entero'] = DECIMAL
 
 # Sums
-cubeDict['int+int'] = INT
-cubeDict['float+int'] = FLOAT
-cubeDict['int+float'] = FLOAT
-cubeDict['float+float'] = FLOAT
+cubeDict['entero+entero'] = ENTERO
+cubeDict['decimal+entero'] = DECIMAL
+cubeDict['entero+decimal'] = DECIMAL
+cubeDict['decimal+decimal'] = DECIMAL
 
 # Substraction
-cubeDict['int-int'] = INT
-cubeDict['float-int'] = FLOAT
-cubeDict['float-float'] = FLOAT
-cubeDict['int-float'] = FLOAT
+cubeDict['entero-entero'] = ENTERO
+cubeDict['decimal-entero'] = DECIMAL
+cubeDict['decimal-decimal'] = DECIMAL
+cubeDict['entero-decimal'] = DECIMAL
 
 # Multiplication
-cubeDict['int*int'] = INT
-cubeDict['int*float'] = FLOAT
-cubeDict['float*int'] = FLOAT
-cubeDict['float*float'] = FLOAT
+cubeDict['entero*entero'] = ENTERO
+cubeDict['entero*decimal'] = DECIMAL
+cubeDict['decimal*entero'] = DECIMAL
+cubeDict['decimal*decimal'] = DECIMAL
 
 # Division
-cubeDict['int/int'] = INT
-cubeDict['int/float'] = FLOAT
-cubeDict['float/int'] = FLOAT
-cubeDict['float/float'] = FLOAT
+cubeDict['entero/entero'] = ENTERO
+cubeDict['entero/decimal'] = DECIMAL
+cubeDict['decimal/entero'] = DECIMAL
+cubeDict['decimal/decimal'] = DECIMAL
 
 # Less than
-cubeDict['int<int'] = BOOL
-cubeDict['int<float'] = BOOL
-cubeDict['float<int'] = BOOL
-cubeDict['float<float'] = BOOL
+cubeDict['entero<entero'] = BOOL
+cubeDict['entero<decimal'] = BOOL
+cubeDict['decimal<entero'] = BOOL
+cubeDict['decimal<decimal'] = BOOL
 
 # Less or equal than
-cubeDict['int<=int'] = BOOL
-cubeDict['int<=float'] = BOOL
-cubeDict['float<=int'] = BOOL
-cubeDict['float<=float'] = BOOL
+cubeDict['entero<=entero'] = BOOL
+cubeDict['entero<=decimal'] = BOOL
+cubeDict['decimal<=entero'] = BOOL
+cubeDict['decimal<=decimal'] = BOOL
 
 # Equals
-cubeDict['int==int'] = BOOL
-cubeDict['int==float'] = BOOL
-cubeDict['float==int'] = BOOL
-cubeDict['float==float'] = BOOL
+cubeDict['entero==entero'] = BOOL
+cubeDict['entero==decimal'] = BOOL
+cubeDict['decimal==entero'] = BOOL
+cubeDict['decimal==decimal'] = BOOL
 cubeDict['bool==bool'] = BOOL
-cubeDict['string==string'] = BOOL
+cubeDict['cadena==cadena'] = BOOL
 
 # Greater than
-cubeDict['int>int'] = BOOL
-cubeDict['int>float'] = BOOL
-cubeDict['float>int'] = BOOL
-cubeDict['float>float'] = BOOL
+cubeDict['entero>entero'] = BOOL
+cubeDict['entero>decimal'] = BOOL
+cubeDict['decimal>entero'] = BOOL
+cubeDict['decimal>decimal'] = BOOL
 
 # Greater or equal than
-cubeDict['int>=int'] = BOOL
-cubeDict['int>=float'] = BOOL
-cubeDict['float>=int'] = BOOL
-cubeDict['float>=float'] = BOOL
+cubeDict['entero>=entero'] = BOOL
+cubeDict['entero>=decimal'] = BOOL
+cubeDict['decimal>=entero'] = BOOL
+cubeDict['decimal>=decimal'] = BOOL
 
 # Different
-cubeDict['int!=int'] = BOOL
-cubeDict['int!=float'] = BOOL
-cubeDict['float!=int'] = BOOL
-cubeDict['float!=float'] = BOOL
-cubeDict['string!=string'] = BOOL
+cubeDict['entero!=entero'] = BOOL
+cubeDict['entero!=decimal'] = BOOL
+cubeDict['decimal!=entero'] = BOOL
+cubeDict['decimal!=decimal'] = BOOL
+cubeDict['cadena!=cadena'] = BOOL
 cubeDict['bool!=bool'] = BOOL
 
 # And
